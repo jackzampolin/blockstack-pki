f7ce093af7ee161e9e334ec50c4d02d0c8a80ff7
cwackerfuss <cwackerfuss@gmail.com>
2018-03-29 13:57:55 -0500

    made many updates to index.js and added apps and app node generation

--- index.js
+++ index.js
@@ -2,55 +2,186 @@ var blockstack = require('blockstack')
 const keychains = require('blockstack-keychains')
 const bitcoin = require('bitcoinjs-lib')
 const bip39 = require('bip39')
+const crypto = require('crypto')
 
-class IdentityNode{
-    constructor(key){
-        this.key = key
-    }
-    getAddress(){
-        return this.key.keyPair.getAddress()
-    }
-    getSKHex(){
-        return this.key.keyPair.d.toBuffer(32).toString('hex')
-    }
-    getHex(){
-      return this.key.keyPair.d.toHex() + '01'
-    }
+// Simple hashing function
+function hashCode(string) {
+  let hash = 0
+  if (string.length === 0) return hash
+  for (let i = 0; i < string.length; i++) {
+    const character = string.charCodeAt(i)
+    hash = (hash << 5) - hash + character
+    hash = hash & hash
+  }
+  return hash & 0x7fffffff
 }
 
-console.log("Generating a new mnomonic and then deriving keys with explanations")
+// Base class for HD (hierarchal deterministic) nodes
+class HdNode {
+  constructor(node) {
+    this.node = node
+  }
+  getAddress(){
+    return this.node.keyPair.getAddress()
+  }
+  getSKHex(){
+    return this.node.keyPair.d.toBuffer(32).toString('hex')
+  }
+  getHex(){
+    return this.node.keyPair.d.toHex() + '01'
+  }
+}
 
-let keychain = new keychains.PrivateKeychain()
+///////////////////////
+// Mnemonic
+///////////////////////
+
+console.log("Generating a new mnemonic and then deriving keys with explanations")
 
 var mnemonic = "slam cheap sponsor average issue lemon nuclear file gesture snake other seminar"
 
-// console.log("  mnemonic:", keychain.mnemonic())
-console.log("  mnemonic:", mnemonic)
+console.log(`
+==== Mnemonic ====
+${ mnemonic }
+`)
 
-var getMaster = (mnemonic) => { return bitcoin.HDNode.fromSeedBuffer(bip39.mnemonicToSeed(mnemonic)) }
- 
-// var master = getMaster(keychain.mnemonic())
-var master = getMaster(mnemonic)
+///////////////////////
+// Master Node Generation
+///////////////////////
 
-console.log("  Root Private Key hardened:", master.keyPair.d.toHex() + '01')
+class MasterNode extends HdNode {
+  constructor(node){
+    super(node)
+  }
+  getWalletNode() {
+    // Derive browser wallet node at `44'/0'/0'/0/0`
+    return new WalletNode(this.node.deriveHardened(44).deriveHardened(0).deriveHardened(0).derive(0).derive(0))
+  }
+  getIdentityNode(index) {
+    // Derive browser wallet node at `888'/0'/index'`
+    return new IdentityNode(this.node.deriveHardened(888).deriveHardened(0).deriveHardened(index))
+  }
+}
 
-// Derive browser wallet key at `44/0/0`
-var getBTC = (master) => { return master.deriveHardened(44).deriveHardened(0).deriveHardened(0).derive(0).derive(0) }
+const createMasterNode = (mnemonic) => {
+  return new MasterNode(
+    bitcoin.HDNode.fromSeedBuffer(bip39.mnemonicToSeed(mnemonic))
+  )
+}
 
-let btcKey = getBTC(master)
+var myMasterNode = createMasterNode(mnemonic)
 
-console.log("  Browser Bitcoin Wallet Address:", btcKey.keyPair.getAddress())
-console.log("  Browser Bitcoin Wallet PrivateKey:", btcKey.keyPair.d.toHex() + '01')
+console.log(`
+==== Master Node ====
+- Address:
+${myMasterNode.getAddress()}
+- PrivateKey:
+${myMasterNode.getSKHex()}
+`)
 
-// Derive Identity keys at `888/0/index`
-var getIdentityKeyCurrent = (master, index) => { return new IdentityNode(master.deriveHardened(888).deriveHardened(0).deriveHardened(index)) }
 
-let id0 = getIdentityKeyCurrent(master, 0)
+///////////////////////
+// Wallet Node Generation
+///////////////////////
 
-console.log("  ID0 Identity Address:", id0.getAddress())
-console.log("  ID0 Identity PKHex:", id0.getHex())
+class WalletNode extends HdNode {
+  constructor(node){
+    super(node)
+  }
+}
 
-let id1 = getIdentityKeyCurrent(master, 1)
+const myWalletNode = myMasterNode.getWalletNode()
 
-console.log("  ID1 Identity Address:", id1.getAddress())
-console.log("  ID1 Identity PKHex:", id1.getHex())
+console.log(`
+==== Bitcoin Wallet Node ====
+- Address:
+${myWalletNode.getAddress()}
+- PrivateKey:
+${myWalletNode.getSKHex()}
+`)
+
+
+///////////////////////
+// Identity Node Generation
+///////////////////////
+
+class IdentityNode extends HdNode {
+  constructor(node){
+    super(node)
+  }
+  getAppsNode() {
+    return new AppsNode(this.node.deriveHardened(0))
+  }
+}
+
+const createIdentityNode = index => (
+  myMasterNode.getIdentityNode(index)
+)
+
+const myIdentityNode = createIdentityNode(0)
+
+console.log(`
+==== Identity 1 Node (You can make many) ====
+- Address:
+${myIdentityNode.getAddress()}
+- PrivateKey:
+${myIdentityNode.getSKHex()}
+`)
+
+
+///////////////////////
+// Apps Node Generation
+///////////////////////
+
+class AppsNode extends HdNode {
+  constructor(node){
+    super(node)
+  }
+  getAppNode(appDomain) {
+    const hash = crypto
+      .createHash('sha256')
+      .update(appDomain)
+      .digest('hex')
+    const appIndex = hashCode(hash)
+    const appNode = this.node.deriveHardened(appIndex)
+    return new AppNode(appNode, appDomain)
+  }
+}
+
+const myAppsNode = myIdentityNode.getAppsNode()
+
+console.log(`
+==== Identity 1's Apps Node  ====
+- Address:
+${myAppsNode.getAddress()}
+- PrivateKey:
+${myAppsNode.getSKHex()}
+`)
+
+
+///////////////////////
+// App Node Generation
+///////////////////////
+
+class AppNode extends HdNode {
+  constructor(node, appDomain) {
+    super(node)
+  }
+  getAppDomain() {
+    return this.appDomain
+  }
+}
+
+const createAppNode = domain => (
+  myAppsNode.getAppNode(domain)
+)
+
+const myAppNode = createAppNode('https://www.foo.com')
+
+console.log(`
+==== Identity 1's "https://www.foo.com" App Node  ====
+- Address:
+${myAppsNode.getAddress()}
+- PrivateKey:
+${myAppsNode.getSKHex()}
+`)
